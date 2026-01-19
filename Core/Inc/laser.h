445e000d9af5bf95819e6963cdbecdc8e90e9e64
Xiang xin wang <wxinxiang8@gmail.com>
2026-01-19 16:51:43 +0800

    添加文件

---
+++ Core/Inc/laser.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: Xiang xin wang wxinxiang8@gmail.com
+ * @Date: 2026-01-19 16:39:33
+ * @LastEditors: Xiang xin wang wxinxiang8@gmail.com
+ * @LastEditTime: 2026-01-19 16:47:28
+ * @FilePath: \MDK-ARMd:\robot fighting\robot\Core\Inc\laser.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef LASER_H
+#define LASER_H
+
+#endif // LASER_H
