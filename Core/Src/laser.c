7fd3d3cb33dab2123d6e1257e121703b6137c4a1
Xiang xin wang <wxinxiang8@gmail.com>
2026-01-19 16:49:30 +0800

    laser_sensor

---
+++ Core/Src/laser.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: Xiang xin wang wxinxiang8@gmail.com
+ * @Date: 2026-01-19 16:39:25
+ * @LastEditors: Xiang xin wang wxinxiang8@gmail.com
+ * @LastEditTime: 2026-01-19 16:43:17
+ * @FilePath: \MDK-ARMd:\robot fighting\robot\Core\Src\laser.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "laser.h"
