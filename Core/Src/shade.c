25401e900516a8f7f750ff8ff0e7d2916cdaed38
Xiang xin wang <wxinxiang8@gmail.com>
2026-01-22 15:49:20 +0800

    shade and laser test

---
+++ Core/Src/shade.c
@@ -0,0 +1,22 @@
+#include "shade.h"
+#include "adc.h"
+#include "dma.h"
+
+// MX_ADC2_Init();
+// MX_DMA_Init(); // Do not call init functions at global scope. They are called in main.c
+
+uint32_t shade[4];//adc value
+float voltage[4];//voltage value
+
+void site_detect_shade()
+{
+
+    HAL_ADC_Start_DMA(&hadc2,(uint32_t*)shade,4);//start adc2 dma
+    
+    HAL_Delay(100);//wait for adc conversion complete
+
+    for(int i=0;i<4;i++)
+    {
+        voltage[i]=(float)(shade[i]*3.3f)/4095.0f;//convert adc value to voltage value
+    }
+}
